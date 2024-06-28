2570bdbe14eebac78b54014e0026b737bf92ede8
Dushyant2026 <dushyantpandit799@gmail.com>
2024-06-28 21:09:28 +0530

    done

--- project4props/src/App.jsx
+++ project4props/src/App.jsx
@@ -1,10 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [ans, setfun] = useState('');
+
+  function bmifun() {
+    const height = parseFloat(document.getElementById('height').value);
+    const weight = parseFloat(document.getElementById('weight').value);
+
+    if (height <= 1 || height === '' || isNaN(height)) {
+      setfun("ENTER THE RIGHT HEIGHT");
+    }
+    else if (weight <= 1 || weight === '' || isNaN(weight)) {
+      setfun("ENTER THE RIGHT WEIGHT");
+    } 
+    else {
+      const ans = (weight / ((height / 100) ** 2)).toFixed(2);
+      setfun(`Your BMI is ${ans}`);
+
+      if(ans < 18.6){
+        setfun(`YOU UNDER WEIGHT TAKE A CALORIES SURPLUS DEIT BECAUSE YOUR BMI IS : ${ans}`)
+      }
+      else if(ans > 24.9){
+        setfun(`YOU OVER WEIGHT TAKE A CALORIES DEFICITE DEIT BECAUSE YOUR BMI IS : ${ans}`)
+      }
+      else{
+      setfun(`YOUR HEIGHT TO WEIGHT RATIO IS PERFECT MAINTAINED BODY BECAUSE YOUR BMI IS : ${ans}`)
+      }
+    }
+  }
 
   return (
     <>
@@ -22,14 +50,18 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          <label >Height in CM: To Calculate BMI   </label>
+          <input type="text" id="height" />
         </p>
+        <p>
+          <label >Weight in KG: To Calculate BMI   </label>
+          <input type="text" id="weight" />
+        </p>
+        <button className = "bmi-button" onClick={bmifun}>CALCULATE THE BMI IS</button>
+        <p>{ans}</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
