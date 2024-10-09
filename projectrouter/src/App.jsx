8482fadc713481469cbe61345857b3a266566b97
Dushyant <dushyantpandit799@gmail.com>
2024-10-09 23:31:17 +0530

    update

--- projectrouter/src/App.jsx
+++ projectrouter/src/App.jsx
@@ -6,10 +6,17 @@ import Topic from './components/Topic';
 const App = () => {
   return (
     <Router>
-      <div class="flex justify-center items-start absolute top-0 left-0 w-full"> 
-        <h1 class="bg-purple-500 text-white text-3xl p-4 rounded-lg text-center">DSA Coding Questions</h1>
-        </div>
-        <div>
+      <div className="flex flex-col justify-center items-center w-full py-6 space-y-4 bg-gray-100">
+        <h1 className="bg-purple-600 text-white text-4xl font-bold p-6 rounded-lg shadow-lg text-center w-11/12 md:w-3/4">
+          DATA STRUCTURE AND ALGORITHMS
+        </h1>
+
+        <h2 className="bg-purple-500 text-white text-2xl font-semibold p-5 rounded-lg shadow-lg text-center w-11/12 md:w-3/4">
+          IMPORTANT QUESTIONS ACCORDING TO TOPICS
+        </h2>
+      </div>
+
+      <div>
         <Routes>
           <Route path="/" element={<TopicsList />} />
           <Route path="/topic/:topicId" element={<Topic />} />
