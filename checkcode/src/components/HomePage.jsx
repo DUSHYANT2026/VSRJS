aad1b813732a73c8a997a3d3aa661216a815bf34
Dushyant <dushyantpandit799@gmail.com>
2025-01-05 14:43:46 +0530

    update

--- checkcode/src/components/HomePage.jsx
+++ checkcode/src/components/HomePage.jsx
@@ -1,10 +1,58 @@
-import React from 'react';
+import React, { useState } from "react";
 
-const HomePage = () => {
+const HomePage= () => {
+  const [name, setName] = useState("");
+  const [submittedName, setSubmittedName] = useState("");
+
+  const handleInputChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSubmittedName(name); 
+  };
+
   return (
-    <div>
-      <h1>Welcome to the Code Example App</h1>
-      <p>Click on the "Binary Search Code" link to view the code example.</p>
+    <div style={{ margin: "50px", textAlign: "center" }}>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Enter your name:</label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={handleInputChange}
+            placeholder="Your Name"
+            required
+            style={{
+              margin: "10px",
+              padding: "5px",
+              border: "1px solid #ccc",
+              borderRadius: "4px",
+            }}
+          />
+        </div>
+        <button
+          type="submit"
+          style={{
+            padding: "10px 20px",
+            backgroundColor: "#007BFF",
+            color: "#fff",
+            border: "none",
+            borderRadius: "4px",
+            cursor: "pointer",
+          }}
+        >
+          Submit
+        </button>
+      </form>
+      {submittedName && (
+        <div style={{ marginTop: "20px" }}>
+          <h2>Hello, {submittedName}!</h2>
+        </div>
+      )}
     </div>
   );
 };
--- prorouter/src/components/Header/Header.jsx
+++ prorouter/src/components/Header/Header.jsx
@@ -117,8 +117,6 @@ export default function Header() {
                                 </NavLink>
 
                             </li>
-                            
-
                         </ul>
                     </div>
                 </div>
